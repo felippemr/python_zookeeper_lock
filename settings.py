7b09b9194eecbba3f351b4f071e6b950c9696b1c
felippemr <raposo.felippe@gmail.com>
2016-03-09 04:04:06 -0300

    Add database queue

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
 from kazoo.client import KazooClient
-
+from kazoo.recipe.queue import LockingQueue
 
 ZK_CLIENT = KazooClient()
 ZK_CLIENT.start()
+
+
+DATABASE_QUEUE = LockingQueue(ZK_CLIENT, '/database_queue')
