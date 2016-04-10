fcd41a76e937ca6f0b7426e368e5af2e3fe6d235
felippemr <raposo.felippe@gmail.com>
2016-04-10 17:11:17 -0300

    Improve design

--- src/worker/base.py
+++ src/worker/base.py
@@ -8,22 +8,6 @@ from kazoo.client import KazooClient
 from kazoo.recipe.queue import LockingQueue
 
 
-class Resource(object):
-    def __init__(self, name, zookeeper_client):
-        self._zookeeper_client = zookeeper_client
-        self._name = name
-    @contextmanager
-    def get(self, resource_id, timeout=60.0):
-        lock = self._zookeeper_client.Lock(
-            "/{}".format(self._name), "{}".format(resource_id)
-        )
-        try:
-            lock.acquire(blocking=True, timeout=timeout)
-            yield lock
-        finally:
-            lock.release()
-
-
 
 def worker(*args, name):
     process = multiprocessing.Process(
@@ -32,30 +16,27 @@ def worker(*args, name):
     process.start()
     process.join()
 
+
 def _work(queue_name, function, resource_name, lock_timeout=10):
-    sys.stderr = open('logs/{}.txt'.format(queue_name), 'w')
-
-    zookeeper_client = KazooClient()
-    zookeeper_client.start()
-    queue = LockingQueue(zookeeper_client, queue_name)
-
-    resource_manager = Resource(resource_name, zookeeper_client)
-
-    logger = multiprocessing.log_to_stderr()
-    logger.setLevel(logging.INFO)
+    logger = setup_logger(queue_name)
     logger.info("Worker Started!")
+
+    zookeeper_client = get_zookeeper_client()
+    queue = LockingQueue(zookeeper_client, queue_name)
 
     while True:
         try:
-            message = json.loads(queue.get().decode("utf-8"))
-            logger.info("Processing message: {}".format(message))
+            message = get_next_message(queue)
         except Exception as e:
             logger.exception(
                 "Error while retrieving message from queue: {}".format(e)
             )
 
         try:
-            with resource_manager.get(message['name'], timeout=lock_timeout):
+            with get_resource_lock(
+                resource_name, zookeeper_client, message['name'],
+                timeout=lock_timeout
+            ):
                 if queue.consume():
                     try:
                         function(message, logger)
@@ -67,3 +48,34 @@ def _work(queue_name, function, resource_name, lock_timeout=10):
             logger.info("LockTimeout on {}".format(message['name']))
         except Exception as e:
             logger.exception("Exception: {}".format(e))
+
+
+def setup_logger(queue_name):
+    sys.stderr = open('logs/{}.txt'.format(queue_name), 'w')
+
+    logger = multiprocessing.log_to_stderr()
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def get_zookeeper_client():
+    zookeeper_client = KazooClient()
+    zookeeper_client.start()
+    return zookeeper_client
+
+
+def get_next_message(queue):
+    return json.loads(queue.get().decode("utf-8"))
+
+
+@contextmanager
+def get_resource_lock(name, zookeeper_client, resource_id, timeout=60.0):
+    lock = zookeeper_client.Lock(
+        "/{}".format(name), "{}".format(resource_id)
+    )
+    try:
+        lock.acquire(blocking=True, timeout=timeout)
+        yield lock
+    finally:
+        lock.release()
+
